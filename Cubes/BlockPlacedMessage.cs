2e5c7b9018fdcc880e9343b5c010533b3ed8a470
12090113 <47487266+12090113@users.noreply.github.com>
2023-04-23 23:34:40 -0700

    Fancy thing

--- Cubes/BlockPlacedMessage.cs
+++ Cubes/BlockPlacedMessage.cs
@@ -4,50 +4,14 @@ using UnityEngine;
 
 namespace Cubes
 {
-    public class BlockPlacedMessage : QSBMessage
+    public class BlockPlacedMessage : QSBMessage<(string path, Vector3Int pos, string name, bool destroyAll)>
     {
-        string path;
-        Vector3Int pos;
-        string name;
-        bool destroyAll;
-
-        public BlockPlacedMessage(string path, Vector3Int pos, string name, bool destroyAll = false)
-        {
-            Cubes.modConsole.WriteLine("Sending block " + name + " placed at " + pos + " on " + path);
-            this.path = path;
-            this.pos = pos;
-            this.name = name;
-            this.destroyAll = destroyAll;
-        }
-        public override void Serialize(NetworkWriter writer)
-        {
-            Cubes.modConsole.WriteLine("Serializing: " + writer);
-            base.Serialize(writer);
-            writer.Write(path);
-            writer.Write(pos);
-            writer.Write(name);
-            writer.Write(destroyAll);
-        }
-
-        public override void Deserialize(NetworkReader reader)
-        {
-            Cubes.modConsole.WriteLine("Deserializing: " + reader);
-            base.Deserialize(reader);
-            path = reader.Read<string>();
-            pos = reader.Read<Vector3Int>();
-            name = reader.Read<string>();
-            destroyAll = reader.Read<bool>();
-        }
+        public BlockPlacedMessage(string path, Vector3Int pos, string name, bool destroyAll = false) : base((path, pos, name, destroyAll)) { }
 
         public override void OnReceiveRemote()
         {
-            Cubes.modConsole.WriteLine("Recieved block " + name + " placed at " + pos + " on " + path);
-            Cubes.I.PlaceRemoteBlock(path, pos, name, destroyAll);
+            Cubes.modConsole.WriteLine("Recieved block " + Data.name + " placed at " + Data.pos + " on " + Data.path);
+            Cubes.I.PlaceRemoteBlock(Data.path, Data.pos, Data.name, Data.destroyAll);
         }
-
-        /*public override void OnReceiveLocal()
-        {
-            Cubes.modConsole.WriteLine("Recieved local " + name + " placed at " + pos + " on " + path);
-        }*/
     }
 }
